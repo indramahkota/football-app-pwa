e51ed1bce338b9ee41050c48a5574e7506734882
Indra Mahkota <indramahkota1@gmail.com>
2020-07-09 16:16:54 +0700

    using custom element

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
-import "materialize-css/dist/css/materialize.min.css";
-import "material-icons/iconfont/material-icons.css";
+// import M from "materialize-css/dist/js/materialize.min.js";
+
+import "./styles/materialize-css/dist/css/materialize.min.css";
+import "./styles/material-icons/iconfont/material-icons.css";
 import "./styles/styles.css";
 
-import M from "materialize-css/dist/js/materialize.min.js";
+import "./components/app-bar";
+import "./components/side-bar";
 
-const appBarTemplate = document.createElement("template");
-appBarTemplate.innerHTML = require("./components/app-bar/template.html");
-document.body.appendChild(appBarTemplate.content.cloneNode(true));
-
-const sideBarTemplate = document.createElement("template");
-sideBarTemplate.innerHTML = require("./components/side-bar/template.html");
-document.body.appendChild(sideBarTemplate.content.cloneNode(true));
-
-const modalTemplate = document.createElement("template");
-modalTemplate.innerHTML = require("./components/app-modal/template.html");
-document.body.appendChild(modalTemplate.content.cloneNode(true));
 
 document.addEventListener("DOMContentLoaded", () => {
-  let sidenav = document.querySelectorAll(".sidenav");
-  M.Sidenav.init(sidenav);
-
-  let dropdown = document.querySelectorAll(".dropdown-trigger");
-  M.Dropdown.init(dropdown, {
-    coverTrigger: false,
-  });
-  
+  /* const sideBar = document.querySelector("side-bar");
+  sideBar.initDropdown();
+  console.log(sideBar); */
 });
 
 /* Sementara dinonaktifkan */
