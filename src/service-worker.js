ffee79d3d18c05a55b73181c4d84a13b37038c62
Indra Mahkota <indramahkota1@gmail.com>
2020-07-14 19:49:29 +0700

    memperbaharui filesToCache

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,24 +1,35 @@
+import footballAppConstant from "./scripts/app-constants.js";
+
 const CACHE_NAME = "indramahkota-footballapp-v1";
 const filesToCache = [
+  /* root folder */
   "/",
   "/bundle.js",
+  "/bundle.css",
   "/worker.js",
   "/index.html",
   "/favicon.ico",
   "/manifest.json",
-  "/MaterialIcons-Regular.eot",
-  "/MaterialIcons-Regular.ttf",
-  "/MaterialIcons-Regular.woff",
-  "/MaterialIcons-Regular.woff2",
-  "/bgprofile.webp",
-  "/indra.webp",
-  "/icon_36x36.png",
-  "/icon_48x48.png",
-  "/icon_72x72.png",
-  "/icon_96x96.png",
-  "/icon_144x144.png",
-  "/icon_192x192.png",
-  "/icon_512x512.png"
+
+  /* inside folder /assets/icons/fonticon */
+  "/assets/icons/fonticon/MaterialIcons-Regular.eot",
+  "/assets/icons/fonticon/MaterialIcons-Regular.ttf",
+  "/assets/icons/fonticon/MaterialIcons-Regular.woff",
+  "/assets/icons/fonticon/MaterialIcons-Regular.woff2",
+
+  /* inside folder /assets/icons/images */
+  "/assets/images/bgprofile.webp",
+  "/assets/images/indra.webp",
+  "/assets/images/null-image.jpg",
+
+  /* inside folder /assets/icons/manifest */
+  "/assets/icons/manifest/icon_36x36.png",
+  "/assets/icons/manifest/icon_48x48.png",
+  "/assets/icons/manifest/icon_72x72.png",
+  "/assets/icons/manifest/icon_96x96.png",
+  "/assets/icons/manifest/icon_144x144.png",
+  "/assets/icons/manifest/icon_192x192.png",
+  "/assets/icons/manifest/icon_512x512.png"
 ];
 
 self.addEventListener("install", e => {
