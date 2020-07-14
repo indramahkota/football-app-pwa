fd810841f956138eb9bf234805d75c8f90fe6f25
Indra Mahkota <indramahkota1@gmail.com>
2020-07-14 20:27:35 +0700

    add proxy url

--- src/scripts/app-constants.js
+++ src/scripts/app-constants.js
@@ -1,4 +1,5 @@
 const footballAppConstant = {
+    proxyUrl: "https://indratestapp.herokuapp.com/",
     baseUrl: "https://api.football-data.org/v2/",
     apiKey: "4ad21fead9504a41a72cad5585bf532a"
 }
