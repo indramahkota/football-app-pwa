4e826dc81c485cebd226d4f8acec89efa2c41e05
Indra Mahkota <indramahkota1@gmail.com>
2020-07-22 00:22:09 +0700

    fix code style

--- src/scripts/app-notification.js
+++ src/scripts/app-notification.js
@@ -1,58 +1,58 @@
 const urlBase64ToUint8Array = base64String => {
-    const padding = "=".repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-        .replace(/-/g, "+")
-        .replace(/_/g, "/");
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-    for (let i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
+  const padding = "=".repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+      .replace(/-/g, "+")
+      .replace(/_/g, "/");
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
   }
+  return outputArray;
+}
   
-  if("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker.register("./worker.js")
-        .then(() => {
-          console.log("Pendaftaran ServiceWorker berhasil");
-        })
-        .catch(() => {
-          console.log("Pendaftaran ServiceWorker gagal");
-        });
-    });
-  
-    if("Notification" in window) {
-      Notification.requestPermission()
-        .then(result => {
-          if(result === "denied") {
-            console.log("Fitur notifikasi tidak diijinkan.");
-            return;
-          } else if(result === "default") {
-            console.error("Pengguna menutup kotak dialog permintaan ijin.");
-            return;
-          }
-          if("PushManager" in window) {
-            navigator.serviceWorker.getRegistration()
-              .then(registration =>
-                registration.pushManager.subscribe({
-                  userVisibleOnly: true,
-                  applicationServerKey: urlBase64ToUint8Array("BKrlDhc0zQnMTPFjLdezIyRaxklJTEegifGWrNHhN3TvHkNVSAI-jYwqRUhigqeqG42C3mFnJL7uJ-JUpGzWbCs")
-                })
-              )
-              .then(subscribe => {
-                console.log(`endpoint: "${subscribe.endpoint}", keys: { p256dh: "${btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey("p256dh"))))}", auth: "${btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey("auth"))))}"}`);
+if("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./worker.js")
+      .then(() => {
+        console.log("Pendaftaran ServiceWorker berhasil");
+      })
+      .catch(() => {
+        console.log("Pendaftaran ServiceWorker gagal");
+      });
+  });
+
+  if("Notification" in window) {
+    Notification.requestPermission()
+      .then(result => {
+        if(result === "denied") {
+          console.log("Fitur notifikasi tidak diijinkan.");
+          return;
+        } else if(result === "default") {
+          console.error("Pengguna menutup kotak dialog permintaan ijin.");
+          return;
+        }
+        if("PushManager" in window) {
+          navigator.serviceWorker.getRegistration()
+            .then(registration =>
+              registration.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array("BKrlDhc0zQnMTPFjLdezIyRaxklJTEegifGWrNHhN3TvHkNVSAI-jYwqRUhigqeqG42C3mFnJL7uJ-JUpGzWbCs")
               })
-              .catch(e =>
-                console.error("Tidak dapat melakukan subscribe ", e.message)
-              );
-          } else {
-            console.log("PushManager tidak didukung browser ini.");
-          }
-        });
-    } else {
-      console.log("PushManager tidak didukung browser ini.");
-    }
+            )
+            .then(subscribe => {
+              console.log(`endpoint: "${subscribe.endpoint}", keys: { p256dh: "${btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey("p256dh"))))}", auth: "${btoa(String.fromCharCode.apply(null, new Uint8Array(subscribe.getKey("auth"))))}"}`);
+            })
+            .catch(e =>
+              console.error("Tidak dapat melakukan subscribe ", e.message)
+            );
+        } else {
+          console.log("PushManager tidak didukung browser ini.");
+        }
+      });
   } else {
-    console.log("ServiceWorker tidak didukung browser ini.");
+    console.log("PushManager tidak didukung browser ini.");
   }
+} else {
+  console.log("ServiceWorker tidak didukung browser ini.");
+}
