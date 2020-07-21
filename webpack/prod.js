d5a9c29929e8530605039d974e3f5d96607c6318
Indra Mahkota <indramahkota1@gmail.com>
2020-07-21 20:18:16 +0700

    change comments

--- webpack/prod.js
+++ webpack/prod.js
@@ -3,9 +3,8 @@ const base = require("./base");
 const TerserPlugin = require("terser-webpack-plugin");
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-/* npm install --save-dev @babel/plugin-transform-runtime */
 /* Babel-Polyfill vs Babel-Transform-Runtime */
-/* Summary: If you're building an app, you can use babel-polyfill. If you're building a library, make sure not to use babel-polyfill, and only the transform-runtime. */
+/* If you're building an app, you can use babel-polyfill. If you're building a library, make sure not to use babel-polyfill, and only the transform-runtime. */
 /* ref: https://gist.github.com/CMCDragonkai/7fb6b279bb667f3c194994b2f2ccedae#:~:text=The%20main%20difference%20is%20that,provide%20special%20functions%20like%20array. */
 module.exports = merge(base, {
   mode: "production",
